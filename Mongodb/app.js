bbc2ff8eb17db8f20167f9a75491bde871a55429
Ashishkanojia2 <kanojiaashish02@gmail.com>
2024-04-27 11:23:44 +0530

    CRUD operation wiht ejs

--- Mongodb/app.js
+++ Mongodb/app.js
@@ -1,35 +1,87 @@
+//      SIMPLE (CRUD) OPERATIONIN NODE EXPRESS AND MONGODB
+
+// const express = require("express");
+// const app = express();
+// const usermodal = require("./usermodal");
+
+// app.get("/", (req, res) => {
+//   res.send("hey i am using Express and node");
+// });
+
+// app.get("/create", async (req, res) => {
+//   let CreatedUser = await usermodal.create({
+//     name: "vikram Singh",
+//     email: "VikramSingh02@gmail.com",
+//     username: "vicky Kumar singh",
+//   });
+//   res.send(CreatedUser);
+// });
+// app.get("/read", async (req, res) => {
+//   let readData = await usermodal.find();
+//   // let readData = await usermodal.findOne({username : "Ashish"});
+//   res.send(readData);
+// });
+// app.get("/update", async (req, res) => {
+//   let update = await usermodal.findOneAndUpdate(
+//     { username: "Ashish Kanojia" },
+//     { name: "Ashish" },
+//     { new: true }
+//   );
+//   res.send(update);
+// });
+// app.get("/delete", async (req, res) => {
+//   let deletedata = await usermodal.findOneAndDelete({ name: "Ashihs" });
+//   res.send(deletedata);
+// });
+
+// app.listen(3000);
+
+//
+//
+//
+//
+//
+//
+//
+
+//******************************************************************************************* */
+
+//    ( CRUD ) OPERATIO USING NODE EXPRESS MONGODB WITH EJS AND SERVER SIDE RENDERING
+
 const express = require("express");
 const app = express();
-const usermodal = require("./usermodal");
+const path = require("path");
+
+const usermodal = require("./modals/user");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("hey i am using Express and node");
-});
-
-app.get("/create", async (req, res) => {
-  let CreatedUser = await usermodal.create({
-    name: "vikram Singh",
-    email: "VikramSingh02@gmail.com",
-    username: "vicky Kumar singh",
-  });
-  res.send(CreatedUser);
+  res.render("index");
 });
 app.get("/read", async (req, res) => {
-  let readData = await usermodal.find();
-  // let readData = await usermodal.find({username : "Ashish"});
-  res.send(readData);
+  let readdata = await usermodal.find();
+  res.render("read", { user: readdata });
 });
-app.get("/update", async (req, res) => {
-  let update = await usermodal.findOneAndUpdate(
-    { username: "Ashish Kanojia" },
-    { name: "Ashish" },
-    { new: true }
-  );
-  res.send(update);
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let user = await usermodal.create({
+    name, //if both the side same name hai to me sirf ek bar name likh sakta hu name:name lie assa hai to,
+    email,
+    image,
+  });
+  res.redirect("/read");
+  
 });
-app.get("/delete", async (req, res) => {
-  let deletedata = await usermodal.findOneAndDelete({ name: "Ashihs" });
-  res.send(deletedata);
+app.get("/deleteUser/:id", async (req, res) => {
+    let deleteuser = await usermodal.findOneAndDelete({_id: req.params.id})
+    // res.send("hey",req.params.id)
+    res.redirect("/read")
+    
+//    console.log(req.param.body)
 });
 
 app.listen(3000);
--- Mongodb/usermodal.js
+++ Mongodb/usermodal.js
@@ -9,3 +9,22 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("user", userSchema); // this is Collection .Collection Create using Proural name MEANS 's' add automatically after name user=> users
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+// mongoose.connect(`mongodb://127.0.0.1:27017/nodemongo`);
+
+// const userSchema2 = mongoose.Schema({
+//   FullName: String,
+//   Email: String,
+//   Phone: Number,
+// });
+
+// module.exports = mongoose.model("user", userSchema2);
