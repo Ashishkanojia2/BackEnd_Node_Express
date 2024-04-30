796fb29bd12e4c97ed819f1a613d04ed2a1e9d95
Ashishkanojia2 <kanojiaashish02@gmail.com>
2024-04-30 23:51:19 +0530

    working on backend

--- Auth/app.js
+++ Auth/app.js
@@ -1,16 +1,37 @@
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const app = express();
+// const cookieParser = require("cookie-parser");
+// const express = require("express");
+// const bcrypt = require("bcrypt");
 
-app.use(cookieParser());
+// const jwt = require("jsonwebtoken");
 
-app.get("/", (req, res) => {
-  res.cookie("name", "AshishCookie");
-  res.send("hey guys we are learning Authentaction in MERN");
-});
-app.get("/read", (req, res) => {
-    console.log(req.cookies)
-  res.send("this is Read Page");
-});
+// const app = express();
 
-app.listen(3000);
+// app.use(cookieParser());
+// const saltRounds = 10;
+// app.get("/", (req, res) => {
+//   //   res.cookie("name", "AshishCookie");    **************************** // HERE IS COOKIE CREATED
+//   //   res.send("hey guys we are learning Authentaction in MERN");
+
+//   //   bcrypt.genSalt(saltRounds, function (err, salt) {
+//   //     bcrypt.compare(
+//   //       "Ashishkanojia",
+//   //       "$2b$10$5kUoPGakapNbEyC5JjN6bemBFFvxUJPHbKrNRByZMaHl2EVO/OBFS",
+//   //       function (err, result) {
+//   //         console.log(result);
+//   //       }
+//   //     );
+//   //   });
+
+//   //   const token = jwt.sign({ email: "Ashish@Kanojia" }, "secret");
+//   //   res.cookie("token", token);
+//   //   res.send("working")
+//   let data = jwt.verify(req.cookies.token, "secret");
+//   console.log(data);
+// });
+
+// app.get("/read", (req, res) => {
+//   console.log(req.cookies); //**********************************************// FOR READING COOKIES
+//   res.send("this is Read Page");
+// });
+
+// app.listen(3000);
---
+++ Auth/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const userModal = require("./Module/user");
+const jwt = require("jsonwebtoken");
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const { JsonWebTokenError } = require("jsonwebtoken");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/find", async (req, res) => {
+  let findall = await userModal.find();
+  res.send(findall);
+});
+// app.get("/delete", async (req, res) => {
+//   let findall = await userModal.findOneAndDelete({ _id: req.body.id });
+//   res.send(findall);
+// });
+app.post("/create", (req, res) => {
+  let { username, password, email, phone } = req.body;
+  bcrypt.genSalt(10, function (err, salt) {
+    console.log("here is salt", salt);
+    bcrypt.hash(password, salt, async function (err, hash) {
+      console.log("is Password create", hash);
+      let createdUser = await userModal.create({
+        username,
+        password: hash,
+        email,
+        phone,
+      });
+      let tokenkey = jwt.sign({ email }, "secretKey");
+      res.cookie("Newtoken", tokenkey);
+      res.send(createdUser);
+      console.log(createdUser);
+    });
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("Newtoken", "");
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/login", async (req, res) => {
+  //   res.render("login");
+  let user = await userModal.findOne({ email: req.body.email });
+  console.log("user ", user);
+  if (!user) return res.send("something went wrong");
+  console.log("user Password ->", user.password);
+});
+
+app.listen(3000);
